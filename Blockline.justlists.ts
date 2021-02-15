66e6a4783f15faa961fe480e16ef745a8cab4784
blocklinebot <79070847+blocklinebot@users.noreply.github.com>
2021-02-15 11:30:01 +0530

    Create Blockline.justlists.ts

---
+++ Blockline.justlists.ts
@@ -0,0 +1,26 @@
+{
+    "name": "BLOCKLINE",
+    "logoURI": "https://blockline.cc/Block_line.png",
+    "timestamp": 1613192922,
+    "tokens": [
+        {
+            "chainId": "1003742",
+            "address": "TADbUCJbLH3By7tLR6aLNLQphQK6qqsasW",
+            "name": "BLOCKLINE",
+            "symbol": "BLK",
+            "decimals": "18",
+            "logoURI": "https://blockline.cc/Block_line.png"
+        }
+    ],
+    "version": {
+        "major": "1",
+        "minor": "0",
+        "patch": "0"
+    }
+}
+
+
+
+JustList URL: https://github.com/justswaporg/justlists/Blockline.justlists.ts
+JustList Name: tronconnect.justlists.ts
+Link to the official homepage of the JustList manager: https://github.com/justswaporg/justlists/new/main
